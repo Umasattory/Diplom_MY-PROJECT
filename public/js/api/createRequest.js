09cc4a51772d88e8b62374388b8613a88d97e66c
Umed Sattorov <umasattory@gmail.com>
2023-10-12 19:27:58 +0300

    Diplom JS 12/10

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,33 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+   const xhr = new XMLHttpRequest();
+   xhr.responseType = 'json';
+   const url = options.url;
+   const formData = null;
 
+   if (options.data) {
+      if (options.method === 'GET') {
+         url += '?' + Object.entries(data).map(
+            entry => entry.map(encodeURIComponent).join('=')).join('&');
+      } else {
+         formData = new FormData();
+         Object.entries(data).forEach(it=>formData.append(...it))
+      }
+   }
+
+   xhr.onreadystatechange = function () {
+      const err = null;
+      const response = null;      
+      if (xhr.response?.success) {
+         callback(err, response = xhr.responseText);
+         console.log(response);
+      } else {
+         callback(err = xhr.response, response);
+         console.error(err)
+      }
+   }
+
+   xhr.open(options.method, url);
+   xhr.send(formData)
 };
