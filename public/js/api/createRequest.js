ba72434ecbdd39b91daaa7cae89c4b08e6b6692e
Umed Sattorov <umasattory@gmail.com>
2023-10-13 02:26:25 +0300

    13/10

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -4,7 +4,7 @@
  * */
 const createRequest = (options = {}) => {
    let url = options.url;
-   let formData = null;
+   let formData = null, resp = null, err = null;
 
    const xhr = new XMLHttpRequest();
    xhr.responseType = 'json';
@@ -20,13 +20,13 @@ const createRequest = (options = {}) => {
    try {
       xhr.open(options.method, url);
       xhr.send(formData);
-
-      console.log("Вариант Основной")
+      resp = xhr.response;
    } catch (error) {
-      
+      err = xhr.error;
       console.log("Ошибка данных!");
    }
 
+   options.callback(err, resp);
    xhr.onerror = (e) => {
       throw Error("Нет данных. Ошибка соединения -" + e);
    }
