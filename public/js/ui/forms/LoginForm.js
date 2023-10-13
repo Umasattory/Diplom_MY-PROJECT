dfed4f254c944bebadbfdf2f0e20938296da0f05
Umed Sattorov <umasattory@gmail.com>
2023-10-13 17:21:20 +0300

    13/10

--- public/js/ui/forms/LoginForm.js
+++ public/js/ui/forms/LoginForm.js
@@ -10,12 +10,15 @@ class LoginForm extends AsyncForm {
    * закрывает окно, в котором находится форма
    * */
   onSubmit(data) {
-    this.element.reset();
+    console.log(data)
     User.login(data, (err, response) => {
+      console.log(response)
       if (response && response.success) {
         this.element.reset();
         App.setState('user-logged');
         App.setModal('login').close();
+      } else {
+        this.element.reset();
       };
     })
   }
